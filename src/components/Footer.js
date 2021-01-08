63b1123589630423cf8888ad5893fbcc50d26dbe
brockmolmen <brockmolmen@gmail.com>
2021-01-07 19:38:08 -0800

    about page design worked

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faFilePdf } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+
 
 const Footer = () => {
   return <div className="Footer">
-  <p>Footer Here!</p>
+  <ul>
+  <li><FontAwesomeIcon icon={faLinkedinIn} /></li>
+  <li><FontAwesomeIcon icon={faGithub} /></li>
+  <li><FontAwesomeIcon icon={faEnvelope} /></li>
+  <li><FontAwesomeIcon icon={faFilePdf} /></li>
+  </ul>
   </div>
 }
 
 export default Footer;
+
