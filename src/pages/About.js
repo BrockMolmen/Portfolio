3099ac673e2e12faa9c0050b061d410934a0b37d
brockmolmen <brockmolmen@gmail.com>
2021-01-10 18:37:54 -0800

    okay, one last one

--- src/pages/About.js
+++ src/pages/About.js
@@ -30,14 +30,17 @@ const About = () => {
             <h1 className="hello">Hello!</h1>
           </div>
           <img src="BrockMolmen.jpg" alt="Brock Molmen" className="bio-pic" />
+        <div className="email">
+          <p>BrockMolmen@gmail.com</p>
+        </div>
         </div>
         <div className="proj-right">
           <div className="center-it">
             <ul className="about-links">
-              <li><a href="https://www.linkedin.com/in/brockmolmen/" target="_blank" alt="LinkedIn" title="LinkedIn"><FontAwesomeIcon icon={faLinkedinIn}  className="icon-1" /></a></li>
-              <li><a href="https://github.com/BrockMolmen" target="_blank" alt="GitHub" title="GitHub"><FontAwesomeIcon icon={faGithub}  className="icon-2" /></a></li>
-              <li><a href="mailto:BrockMolmen@gmail.com" alt="Email" title="Email"><FontAwesomeIcon icon={faEnvelope}  className="icon-3" /></a></li>
-              <li><a href="BrockMolmen_resume.pdf" target="_blank" alt="Resume" title="Resume"><FontAwesomeIcon icon={faFilePdf}  className="icon-4" /></a></li>
+              <li><a href="https://www.linkedin.com/in/brockmolmen/" target="_blank" alt="LinkedIn" title="LinkedIn"><FontAwesomeIcon icon={faLinkedinIn} className="icon-1" /></a></li>
+              <li><a href="https://github.com/BrockMolmen" target="_blank" alt="GitHub" title="GitHub"><FontAwesomeIcon icon={faGithub} className="icon-2" /></a></li>
+              <li><a href="mailto:BrockMolmen@gmail.com" alt="Email" title="Email"><FontAwesomeIcon icon={faEnvelope} className="icon-3" /></a></li>
+              <li><a href="BrockMolmen_resume.pdf" target="_blank" alt="Resume" title="Resume"><FontAwesomeIcon icon={faFilePdf} className="icon-4" /></a></li>
             </ul>
           </div>
           <p>My name is Brock. I am a full-stack software engineer, focused on innovative front-end development. I enjoy writing code that looks as good as it functions. Transitioning from a career in the ever-evolving airline industry, I’m able to adapt and learn quickly in new situations and rely on great communication to accomplish goals in a team.</p>
